9beef2ce1cd6d6c0f594eb3f68736ba6ceb06750
Safar Safarov <Safarov.safar@hotmail.com>
2022-06-15 15:18:16 +0500

    123

--- question/app.js
+++ question/app.js
@@ -1,2 +1,15 @@
-//using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function () {
+    const btn = question.querySelector(".question-btn");
+
+    btn.addeventListener("click", () => {
+        questions.forEach(function (time) {
+            if (item !== question) {
+                item.classList.remove("show-text")
+            }
+        });
+
+        question.classList.toggle("show-text");
+    });
+});
