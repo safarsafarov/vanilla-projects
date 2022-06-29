b2e20f868a6467b3f971364d26365c252aea8b34
Safar Safarov <Safarov.safar@hotmail.com>
2022-06-29 11:44:59 +0500

    commit

--- dictionary-app/script.js
+++ dictionary-app/script.js
@@ -1,3 +1,13 @@
 const wrapper = document.querySelector(".wrapper");
 searchInput - wrapper.querySelector("input");
 volume = wrapper.querySelector("input");
+infoText = wrapper.querySelector(".info-text");
+synonyms = wrapper.querySelector(".synonyms .list");
+removeIcon = wrapper.querySelector(".search span");
+let audio;
+
+function data(result, word) {
+    if(result.title) {
+        infoText.innerHTML = "Can't "
+    }
+}
