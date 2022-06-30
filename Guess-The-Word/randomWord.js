46fe56053e6bb82d1eb2dccd63a49a2827e284a7
Safar Safarov <Safarov.safar@hotmail.com>
2022-06-30 10:27:26 +0500

    1

---
+++ Guess-The-Word/randomWord.js
@@ -0,0 +1,15 @@
+function randomWord() {
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word;
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    hintTag.innerText = ranItem.hint;
+    guessLeft.innerText = maxGuesses;
+    wrongLetter.innerText = incorrectLetters;
+
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
--- Guess-The-Word/script.js
+++ Guess-The-Word/script.js
@@ -7,21 +7,6 @@ typingInput = document.querySelector(".typing-input");
 
 let word, maxGuesses, incorrectLetters = [], correctLetters = [];
 
-function randomWord() {
-    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
-    word = ranItem.word;
-    maxGuesses = word.length >= 5 ? 8 : 6;
-    correctLetters = []; incorrectLetters = [];
-    hintTag.innerText = ranItem.hint;
-    guessLeft.innerText = maxGuesses;
-    wrongLetter.innerText = incorrectLetters;
-
-    let html = "";
-    for (let i = 0; i < word.length; i++) {
-        html += `<input type="text" disabled>`;
-        inputs.innerHTML = html;
-    }
-}
 randomWord();
 
 
