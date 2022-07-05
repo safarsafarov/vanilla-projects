aac8b6fb6c3fcf593ae9fa60a78edadd895babd7
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-05 11:10:34 +0500

    /

--- Projs/typer/js/script.js
+++ Projs/typer/js/script.js
@@ -15,27 +15,26 @@ function loadParagraph() {
     const ranIndex = Math.floor(Math.random() * paragraphs.length);
     typingText.innerHTML = "";
     paragraphs[ranIndex].split("").forEach(char => {
-        let span = `<span>${char}</span>`;
+        let span = `<span>${char}</span>`
         typingText.innerHTML += span;
     });
-
     typingText.querySelectorAll("span")[0].classList.add("active");
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
 }
 
 function initTyping() {
-    let characters = typingText.querySelectorAll('span');
+    let characters = typingText.querySelectorAll("span");
     let typedChar = inpField.value.split("")[charIndex];
     if(charIndex < characters.length - 1 && timeLeft > 0) {
         if(!isTyping) {
             timer = setInterval(initTimer, 1000);
             isTyping = true;
         }
-        if(typedChar === null) {
-            if(charindex > 0) {
+        if(typedChar == null) {
+            if(charIndex > 0) {
                 charIndex--;
-                if(characters[charIndex].classList.contains("incorrent")) {
+                if(characters[charIndex].classList.contains("incorrect")) {
                     mistakes--;
                 }
                 characters[charIndex].classList.remove("correct", "incorrect");
@@ -45,20 +44,48 @@ function initTyping() {
                 characters[charIndex].classList.add("correct");
             } else {
                 mistakes++;
-                characters[charIndex].classList.add("incorrent");
+                characters[charIndex].classList.add("incorrect");
             }
-            charindex++;
+            charIndex++;
         }
         characters.forEach(span => span.classList.remove("active"));
         characters[charIndex].classList.add("active");
 
         let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
-        wpm = wpm < 0 || !wpm === infinity ? 0 : wpm;
-
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+        
         wpmTag.innerText = wpm;
         mistakeTag.innerText = mistakes;
         cpmTag.innerText = charIndex - mistakes;
     } else {
-        clear
+        clearInterval(timer);
+        inpField.value = "";
+    }   
+}
+
+function initTimer() {
+    if(timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes)  / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
     }
 }
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inpField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inpField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
