15db421c93c0a2c9298d6cd92355db3dda98c79d
Safar Safarov <Safarov.safar@hotmail.com>
2022-07-07 15:47:07 +0500

    /

---
+++ Projs/price-range-slider/script.js
@@ -0,0 +1,21 @@
+const rangeInput = document.querySelectorAll(".range-input input");
+priceInput = document.querySelectorAll(".price-input input");
+range = document.querыSelector(".slider .progress");
+let priceGap = 1000;
+
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        
+        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+            }else{
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
